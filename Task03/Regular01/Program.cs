299322f2c89a58e17f49ce37f9f7f2ff08971058
Kokhanchik Anton <kokhanchik.tversu@gmail.com>
2016-03-08 21:39:43 +0300

    init commit

---
+++ Task03/Regular01/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Regular01                //   Программа будет проверять корректность ввода логина. Корректным логином будет строка от 2-х до
+                                   //   10-ти символов, содержащая только буквы и цифры, и при этом цифра не может быть первой
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
