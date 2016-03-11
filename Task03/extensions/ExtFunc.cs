9aebb74174d904474f36c4a7c85741ff63884cf8
Kokhanchik Anton <kokhanchik.tversu@gmail.com>
2016-03-11 20:14:20 +0300

    added ExtFunc.cs

---
+++ Task03/extensions/ExtFunc.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace extensions
+{
+    static class ExtFunc
+    {
+        public static string GetString<T>(this IList<T> list)
+        {
+            StringBuilder result = new StringBuilder();
+            foreach (T elem in list)
+                result = result.AppendFormat("{0}, ", elem);
+            result.Remove(result.Length - 2, 2);
+            return result.ToString();
+        }
+    }
+}
